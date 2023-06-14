707102730f5dc43e1ded1720c021f7f4d63b1c96
Pavlo-V-M <letter.screen.1@gmail.com>
2023-06-14 11:41:45 +0300

    try-4

--- src/components/GetContent/GetTopMovies.js
+++ src/components/GetContent/GetTopMovies.js
@@ -3,7 +3,7 @@ const api_Key =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZmIwZjA2NDE5YzM3N2Y5MjU1OGEyMjM2Mjc0MTQ5NSIsInN1YiI6IjYxZTZiZjI3YjdhYmI1MDAxYTI1MWZhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.StYavZ4LzGmhemX6cHksaIzE34BKhIiazMjEt5zPnEE';
 const options = {
   method: 'GET',
-  url: 'https://api.themoviedb.org/3/trending/all/day?api_key=e3d680b6e1d63d40d03962db80e2a1a0rfvb ',
+  url: 'https://api.themoviedb.org/3/trending/all/day?api_key=9fb0f06419c377f92558a22362741495 ',
   params: { language: 'en-US' },
   headers: { accept: 'application/json', Authorization: `Bearer ${api_Key}` },
 };
