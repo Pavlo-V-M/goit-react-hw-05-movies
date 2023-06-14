956b301807436d9906332f4bc6a193e6cdc6010d
Pavlo-V-M <letter.screen.1@gmail.com>
2023-06-14 12:06:19 +0300

    try-5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import { lazy } from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 import css from './Home/home.module.css';
 
-const Movies = lazy(() => import('./Movies/Movies'));
-const Layout = lazy(() => import('./Layout/Layout'));
-const Home = lazy(() => import('./Home/Home'));
-const MovieDetails = lazy(() => import('./Movies/MovieDetails'));
-const MovieCast = lazy(() => import('./Movies/MoviesCast'));
-const MoviesReviews = lazy(() => import('./Movies/MoviesReviews'));
+const Movies = () => import('./Movies/Movies');
+const Layout = () => import('./Layout/Layout');
+const Home = () => import('./Home/Home');
+const MovieDetails = () => import('./Movies/MovieDetails');
+const MovieCast = () => import('./Movies/MoviesCast');
+const MoviesReviews = () => import('./Movies/MoviesReviews');
 
 export const App = () => {
   return (
@@ -25,6 +25,34 @@ export const App = () => {
     </div>
   );
 };
+
+// import { lazy } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import css from './Home/home.module.css';
+
+// const Movies = lazy(() => import('./Movies/Movies'));
+// const Layout = lazy(() => import('./Layout/Layout'));
+// const Home = lazy(() => import('./Home/Home'));
+// const MovieDetails = lazy(() => import('./Movies/MovieDetails'));
+// const MovieCast = lazy(() => import('./Movies/MoviesCast'));
+// const MoviesReviews = lazy(() => import('./Movies/MoviesReviews'));
+
+// export const App = () => {
+//   return (
+//     <div className={css.app}>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="movies" element={<Movies />} />
+//           <Route path="movies/:movieId" element={<MovieDetails />}>
+//             <Route path="cast" element={<MovieCast />} />
+//             <Route path="reviews" element={<MoviesReviews />} />
+//           </Route>
+//         </Route>
+//       </Routes>
+//     </div>
+//   );
+// };
 
 // --------------------------------------------------------
 
